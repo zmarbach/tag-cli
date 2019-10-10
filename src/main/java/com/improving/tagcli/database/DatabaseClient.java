ed03c7f2e56f3b2e0078441dbf7977305d563a35
Zach Marbach <Zach.Marbach@improving.com>
2019-10-10 10:19:47 -0500

    commit

--- src/main/java/com/improving/tagcli/database/DatabaseClient.java
+++ src/main/java/com/improving/tagcli/database/DatabaseClient.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class DatabaseClient {
